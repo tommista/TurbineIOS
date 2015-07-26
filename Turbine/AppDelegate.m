9aad87940a23d3912a8679e589886af44f80885a
Tommy Brown <thomasbrown73095@gmail.com>
2015-07-26 11:37:46 -0500

    Removed ViewController, made TweetListViewController the root view controller

--- Turbine/AppDelegate.m
+++ Turbine/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
+#import "TweetListViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,11 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    //ViewController *viewController = [[ViewController alloc] initWithNibName:@"MyViewController" bundle:nil];
-    ViewController *viewController = [[ViewController alloc] init];
-    self.window.rootViewController = viewController;
+    TweetListViewController *tweetListVC = [[TweetListViewController alloc] initWithNibName:@"TweetListViewController" bundle:nil];
+    self.window.rootViewController = tweetListVC;
     [self.window makeKeyAndVisible];
     return YES;
 }
