f5c5c406fd057efdb141aabd163259868fc3f568
Tommy Brown <thomasbrown73095@gmail.com>
2015-08-24 15:27:38 -0500

    Added DBManager

---
+++ Turbine/Managers/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  Turbine
+//
+//  Created by Tommy Brown on 8/24/15.
+//  Copyright (c) 2015 tommista. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ Turbine/Managers/DBManager.m
@@ -0,0 +1,16 @@
+//
+//  DBManager.m
+//  Turbine
+//
+//  Created by Tommy Brown on 8/24/15.
+//  Copyright (c) 2015 tommista. All rights reserved.
+//
+
+#import "DBManager.h"
+#import <sqlite3.h>
+
+@implementation DBManager
+
+
+
+@end
