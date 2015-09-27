300b6130917901fc2595349f1338fee6b02d6d2a
Tommy Brown <thomasbrown73095@gmail.com>
2015-09-27 04:54:16 -0500

    Added SettingsViewController

---
+++ Turbine/ViewControllers/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Turbine
+//
+//  Created by Tommy Brown on 9/27/15.
+//  Copyright (c) 2015 tommista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ Turbine/ViewControllers/SettingsViewController.m
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.m
+//  Turbine
+//
+//  Created by Tommy Brown on 9/27/15.
+//  Copyright (c) 2015 tommista. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
